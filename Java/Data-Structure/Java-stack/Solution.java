0dfc14dbadf719c7d24fb77cdaa711e1d703c33f
gripjun <gripjun@gmail.com>
2016-07-20 17:34:00 +0900

    Create Solution.java
    Hackerrank Java Stack Problem, get a help from a discussion.
    
    https://www.hackerrank.com/challenges/java-stack

---
+++ Java/Data-Structure/Java-stack/Solution.java
@@ -0,0 +1,60 @@
+import java.util.ArrayDeque;
+//Complete this code or write your own from scratch
+import java.util.Deque;
+import java.util.Scanner;
+
+class Solution {
+	
+	static Deque<Character> stack;
+	
+	public Solution() {
+		stack = new ArrayDeque<Character>();
+	}
+	
+	public static boolean isBalanced(String exp) {
+		final int len = exp.length();
+		boolean failed = false;
+		
+		for(int i = 0; !failed && i < len; i++) {
+			char curChar = exp.charAt(i);
+			char counter = '.';
+			switch(curChar) {
+				case '(':
+				case '{':
+				case '[':
+					stack.push(curChar);
+					break;
+					
+					
+				case ')':
+					counter = '(';
+				case '}':
+					if(counter == '.') counter = '{';
+				case ']':
+					if(counter == '.') counter = '[';
+					if(stack.isEmpty() || stack.pop() != counter) {
+						failed = true;
+					}
+					break;
+			}
+		}
+		
+		return stack.isEmpty() && !failed;
+		
+		
+		
+	}
+	
+	public static void main(String[] argh) {
+
+		Scanner in = new Scanner(System.in);
+		
+		
+		while(in.hasNextLine()) {
+			String exp = in.nextLine();
+			System.out.println(isBalanced(exp));
+			stack.clear();
+		}
+		
+	}
+}
